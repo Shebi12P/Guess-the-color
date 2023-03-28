57d9bcf3ac7937ea068c2e1c4fe4e4067638460f
Szymon Brodawka <szymbrod@gmail.com>
2023-03-28 19:08:34 +0200

    Adding rgb colors generation and inserting colors

---
+++ script.js
@@ -0,0 +1,45 @@
+const colorArea = document.getElementById('color-area');
+const colorButtons = document.querySelectorAll(".button-group button");
+
+const genererateRBGColor = () => {
+    const MAX_VALUE = 256;
+    const red = Math.floor(Math.random()*MAX_VALUE);
+    const green = Math.floor(Math.random()*MAX_VALUE);
+    const blue = Math.floor(Math.random()*MAX_VALUE);
+
+    return {
+        'red': red,
+        'green': green,
+        'blue': blue
+    }
+}
+
+
+const rightColor = genererateRBGColor();
+const color2 = genererateRBGColor();
+const color3 = genererateRBGColor();
+const colors = [rightColor, color2, color3];
+
+const insertRGBColor = (rightColor) => {
+    const red = rightColor.red;
+    const green = rightColor.green;
+    const blue = rightColor.blue;
+
+    colorArea.style.setProperty('--bg-color', `rgb(${red}, ${green}, ${blue})`);
+}
+
+const insertButtonColors = () => {
+    colorButtons.forEach((button) => {
+        let randomIndex = Math.floor(Math.random() * colors.length);
+        let randomColor = colors[randomIndex];
+        
+        let red = randomColor.red;
+        let green = randomColor.green;
+        let blue = randomColor.blue;
+        
+        button.textContent = `RGB(${red}, ${green}, ${blue})`;
+
+        let randomColorIndex = colors.indexOf(randomColor);
+        colors.splice(randomColorIndex, 1);
+    })
+}
